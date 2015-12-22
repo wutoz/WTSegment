6af03ac1da3b20fd2d4499c7084484e148b64799
wutongr <zc_and_zc@aliyun.com>
2015-12-22 17:45:21 +0800

    tag 0.1.1

--- WTSegment/ViewController.m
+++ WTSegment/ViewController.m
@@ -30,7 +30,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.data = @[@"标题1",@"标题2",@"标题3",@"标题4",@"标题5",@"标题6",@"标题7",@"标题8",@"标题9",@"标题10",@"标题11"/**/];
+    self.data = @[@"标题1",@"标题2",@"标题3",@"标题4",@"标题5",@"标题6",@"标题7",@"标题8",@"标题9",@"标题10",@"标题11"];
     
     segment = [[WTSegment alloc]initWithFrame:CGRectMake(0, 20, ScreenW, SegmentH) style:WTSegmentStylePlain];
     segment.backgroundColor = [UIColor colorWithHexColor:@"1b2735"];
